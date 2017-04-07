6eaa9be868eac3aece7979df704687f99e2c872d
Ivo Meixner <ivo.meixner@se7en.cz>
2017-04-07 22:53:26 +0200

    Project created

---
+++ CodeLines/FormMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CodeLines
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.openFileDialogSelect = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSelect.Location = new System.Drawing.Point(12, 12);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(160, 97);
+            this.buttonSelect.TabIndex = 0;
+            this.buttonSelect.Text = "Select Repository";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // openFileDialogSelect
+            // 
+            this.openFileDialogSelect.FileName = ".gitignore";
+            this.openFileDialogSelect.Filter = "Git Ignore Files | .gitignore";
+            this.openFileDialogSelect.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogSelect_FileOk);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 121);
+            this.Controls.Add(this.buttonSelect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Code Lines";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.OpenFileDialog openFileDialogSelect;
+    }
+}
+
---
+++ CodeLines/FormMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeLines
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialogSelect.ShowDialog();
+        }
+
+        private void openFileDialogSelect_FileOk(object sender, CancelEventArgs e)
+        {
+            int lastBackslash = openFileDialogSelect.FileName.LastIndexOf('\\');
+            string dirPath = openFileDialogSelect.FileName.Substring(0, lastBackslash);
+        }
+
+        private void processDirectory(string path)
+        {
+
+        }
+    }
+}
